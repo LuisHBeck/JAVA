399c2e4e3a8be62a870c5a0c79c28c18bc20dc66
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-14 09:05:31 -0300

    Update HashMap03.java

--- SENAI/GAMES/EXERCISES/HashMap03.java
+++ SENAI/GAMES/EXERCISES/HashMap03.java
@@ -1,64 +1,67 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class ex03 {
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
 
         List<HashMap<Object, Object>> products = new ArrayList<>();
-        HashMap<Object, Object> product = new HashMap<>();
 
-         while (true){
-             System.out.print("""
+        while (true){
+            System.out.print("""
                  —————————MENU—————————
                  [0] - ADD PRODUCT
-                 [1] - REMOVE PRODUCT
-                 [2] - UPDATE PRODUCT
-                 [3] - SHOW ALL
-                 [4] - EXIT
+                 [1] - SHOW ALL
+                 [2] - REMOVE PRODUCT 
+                 [3] - EXIT
                  >>> """);
-             int resp = tec.nextInt();
-             tec.nextLine();
+            int resp = tec.nextInt();
+            tec.nextLine();
 
-             if (resp == 4){
-                 break;
-             } else if (resp == 0) {
-                 product.clear();
-                 
-                 System.out.print("Product name>> ");
-                 String name = tec.nextLine();
-                 product.put("name", name);
+            if (resp == 3){
+                break;
+            } else if (resp == 0) {
 
-                 System.out.print("Product price>> ");
-                 int price = tec.nextInt();
-                 tec.nextLine();
-                 product.put("price", price);
+                HashMap<Object, Object> product = new HashMap<>();
 
-                 System.out.print("Product quantity>> ");
-                 int quantity = tec.nextInt();
-                 tec.nextLine();
-                 product.put("quantity", quantity);
-                 
-                 products.add(product);
-             } else if (resp == 1) {
-                 int n = 1;
-                 for (HashMap<Object, Object> dicionario: products){
-                     System.out.println();
-                     System.out.println("Product #" + n);
-                     n++;
-                     for (Object key : dicionario.keySet()){
-                         Object value = dicionario.get(key);
-                         System.out.println(key + " = " + value);
-                     }
-                 }
-                 System.out.println("Input the index of product>> ");
-                 int index = tec.nextInt();
-                 tec.nextLine();
-                 if (products.size() >= index){
-                     products.remove(index);
-                 }else {
-                     System.out.println("Invalid index");
-                 }
-             }
-         }
+                System.out.print("Product name>> ");
+                String name = tec.nextLine();
+                product.put("name", name);
+
+                System.out.print("Product price>> ");
+                int price = tec.nextInt();
+                tec.nextLine();
+                product.put("price", price);
+
+                System.out.print("Product quantity>> ");
+                int quantity = tec.nextInt();
+                tec.nextLine();
+                product.put("quantity", quantity);
+
+                products.add(product);
+            } else if (resp == 1) {
+                int n = 1;
+                for (HashMap<Object, Object> dicionario: products){
+                    System.out.println();
+                    System.out.println("Product #" + n);
+                    n++;
+                    for (Object key : dicionario.keySet()){
+                        Object value = dicionario.get(key);
+                        System.out.println(key + " = " + value);
+                    }
+                }
+            } else if (resp == 2) {
+                System.out.print("Input the index of product>> ");
+                int index = tec.nextInt();
+                tec.nextLine();
+                if (products.size() >= index){
+                    products.remove(index - 1);
+                }else {
+                    System.out.println("Invalid index");
+                }
+            }
+        }
     }
 }
