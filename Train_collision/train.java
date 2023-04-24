ce58e04dca466f69b026fc8e79810f5feb21c2c2
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-24 16:41:41 -0300

    Update train.java

--- Train_collision/train.java
+++ Train_collision/train.java
@@ -5,49 +5,64 @@ public class train {
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
 
-        double speedA, speedB, positionA = -1, positionB = 1;
+        double speedA = -1, speedB = 1, positionA = -1, positionB = 1;
 
         while (true){
             try {
                 while (positionA < 0 || positionA > 10000){
-                    System.out.print("Input A's position (0-10000 Km)>> ");
+                    System.out.print("Input A's position (0 - 10000 Km)>> ");
                     positionA = tec.nextDouble();
                 }
                 break;
             }catch (InputMismatchException e){
-                System.out.println("Invalid characetr. Try again");
+                System.out.println("Invalid character. Try again");
             }
             tec.nextLine();
         }
 
+        System.out.println();
+
         while (true){
             try {
-                while (positionB > 0 || positionB < 10000){
-                    System.out.print("Input B's position (0-10000 Km)>> ");
+                while (positionB > 0 || positionB < -10000){
+                    System.out.print("Input B's position (0 - -10000 Km)>> ");
                     positionB = tec.nextDouble();
                 }
                 break;
             }catch (InputMismatchException e){
-                System.out.println("Invalid characetr. Try again");
+                System.out.println("Invalid character. Try again");
             }
             tec.nextLine();
         }
 
+        System.out.println();
 
+        while (true){
+            try {
+                while (speedA < 0 || speedA > 300){
+                    System.out.print("Input A's speed (0 - 300 Km/h)>> ");
+                    speedA = tec.nextDouble();
+                }
+                break;
+            }catch (InputMismatchException e){
+                System.out.println("Invalid character. Try again");
+            }
+            tec.nextLine();
+        }
 
-//        System.out.print("Input B's position>> ");
-//        positionB = tec.nextDouble();
-//        tec.nextLine();
-//
-//        System.out.print("Input A's speed>> ");
-//        speedA = tec.nextDouble();
-//        tec.nextLine();
-//
-//        System.out.print("Input B's speed>> ");
-//        speedB = tec.nextDouble();
-//        tec.nextLine();
+        System.out.println();
 
-
-
+        while (true){
+            try {
+                while (speedB > 0 || speedB < -300){
+                    System.out.print("Input A's speed (0 - 300 Km/h)>> ");
+                    speedB = tec.nextDouble();
+                }
+                break;
+            }catch (InputMismatchException e){
+                System.out.println("Invalid character. Try again");
+            }
+            tec.nextLine();
+        }
     }
 }
