bf2456077340fe29b003b33b32a3bd84b47f4107
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-05-12 09:16:51 -0300

    Create Teste_csv.java

---
+++ CSV/app/Teste_csv.java
@@ -0,0 +1,44 @@
+package Teste.app;
+
+import Teste.entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Teste_csv {
+    public static void main(String[] args) {
+        String path = "C:/Users/47238341840/Desktop/Kalunga_scraping-main/xlsx_archives/Products.csv";
+//        List<Product> productsList = new ArrayList<>();
+        List<String> productList = new ArrayList<>();
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+            line = br.readLine();
+            while (line != null){
+                String[] vec = line.split(";");
+//                int idProduct = Integer.parseInt(vec[0]);
+//                String nameProduct = vec[1];
+//                double priceProduct = Double.parseDouble(vec[2]);
+
+//                Product product = new Product(idProduct, nameProduct, priceProduct);
+                productList.add(Arrays.toString(vec));
+
+                line= br.readLine();
+            }
+            System.out.println("PRODUCTS");
+//            for (Product prod: productsList){
+//                System.out.println(prod);
+//            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        for (String prod: productList){
+            System.out.println(prod);
+            }
+    }
+}
